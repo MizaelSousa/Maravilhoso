6db6cdf15fa6c2a3d12decc0600d9a56a48d68f5
Mizael Sousa <aluno@MacBook-Pro-de-Aluno.local>
2019-02-26 20:27:50 -0400

    first commit in this project

---
+++ Maravilhoso/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Maravilhoso
+{
+    public partial class MainPage : ContentPage
+    {
+        int cont = 1;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void ClicadaDoBotao(Object sender, EventArgs e){
+
+            label_status.Text = "Voce clicou " + cont + " Vezes";
+
+            button_clique.Text = "Novo Botao";
+
+            cont++;
+        }
+    }
+}
